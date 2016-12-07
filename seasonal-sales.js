50e180cc2dbf930ae6648ff9155cc6eb4a19aff2
T-Farmer <terrill@Rels-Pro-186.local>
2016-12-07 06:10:58 -0600

    added div element, framed XMLHttpRequest, renamed file

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,1 +1,8 @@
 console.log("Files are linked");
+
+var myRequest = new XMLHttpRequest()
+
+function loadProducts(e) {
+  var data = JSON.parse(e.target.responseText)
+  console.log("products", data.products)
+}
