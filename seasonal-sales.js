044c832ad90228f3500ba0806b79f1196dd0a55c
T-Farmer <terrill@Rels-Pro-186.local>
2016-12-13 14:26:51 -0600

    parse json and callback

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,8 +1,35 @@
-console.log("Files are linked");
 
-var myRequest = new XMLHttpRequest()
 
-function loadProducts(e) {
-  var data = JSON.parse(e.target.responseText)
-  console.log("products", data.products)
+var items = []
+var departments = []
+
+
+// request for products
+
+var itemRequest = new XMLHttpRequest()
+itemRequest.addEventListener("load", itemRequest)
+itemRequest.open("GET", "products.JSON")
+itemRequest.send()
+
+
+// request for departments
+
+var deptRequest = new XMLHttpRequest()
+deptRequest.addEventListener("load", deptRequest)
+deptRequest.open("GET", "categories.JSON")
+deptRequest.send()
+
+//function to call items during event
+
+function getItems(e) {
+  var presetItems = JSON.parse(e.target.responsetext)
+  items(presetItems)
+}
+
+
+// function to call depts during event
+
+function getDepts(e) {
+  var presetDepts = JSON.parse(e.target.responsetext)
+  departments(presetDepts)
 }
