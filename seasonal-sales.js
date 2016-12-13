97615bb875d47d5c9c6961fd0d4cac3fd0b81113
T-Farmer <terrill@Rels-Pro-186.local>
2016-12-13 15:17:05 -0600

    added css and select element

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -2,34 +2,34 @@
 
 var items = []
 var departments = []
+var presetItems = []
+var presetDepts = []
 
-
-// request for products
+// request for items
 
 var itemRequest = new XMLHttpRequest()
-itemRequest.addEventListener("load", itemRequest)
+itemRequest.addEventListener("load", getItems)
 itemRequest.open("GET", "products.JSON")
-itemRequest.send()
-
-
-// request for departments
-
-var deptRequest = new XMLHttpRequest()
-deptRequest.addEventListener("load", deptRequest)
-deptRequest.open("GET", "categories.JSON")
-deptRequest.send()
+itemRequest.send();
 
 //function to call items during event
 
 function getItems(e) {
   var presetItems = JSON.parse(e.target.responsetext)
-  items(presetItems)
+  console.log("items", presetItems)
 }
 
+
+// request for departments
+
+var deptRequest = new XMLHttpRequest();
+deptRequest.addEventListener("load", getDepts);
+deptRequest.open("GET", "categories.JSON");
+deptRequest.send();
 
 // function to call depts during event
 
 function getDepts(e) {
-  var presetDepts = JSON.parse(e.target.responsetext)
-  departments(presetDepts)
+  var presetDepts = JSON.parse(e.target.responsetext);
+  console.log("departments", presetDepts)
 }
